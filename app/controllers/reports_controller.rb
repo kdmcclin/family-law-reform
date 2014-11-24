2f53be3404e440e651636d03b3427aa7d4182878
Katherine McClintic <kdmcclin@gmail.com>
2014-11-24 13:56:32 -0500

    Add basic routes for reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,11 +1,28 @@
 class ReportsController < ApplicationController
+	
 	before_action :get_report, only: [:show, :edit, :update, :destroy]
+
+	def index
+	end
+
 	def new
 		@report = Report.new
 	end
 
 	def create
 		@report = current_user.reports.new(report_params)
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
 	end
 
 	private
