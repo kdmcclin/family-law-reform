da656af0384b8ca3db8c15fad99cd971745d9f0a
Katherine McClintic <kdmcclin@gmail.com>
2014-11-24 12:29:13 -0500

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+	has_secure_password
+
+	has_many :reports
+
+	validates :email,		presence: true,
+							format: { :with => /\w+@\w+\.\w+/}
+
+	validates :displayname,	presence: true
+
+	validates :password,	length: { minimum: 3 },
+                        	confirmation: true,
+                        	presence: true
+end
