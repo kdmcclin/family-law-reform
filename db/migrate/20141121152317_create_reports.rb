8ff92529bd9f8c03b2721dd965b7695a6a471b27
Katherine McClintic <kdmcclin@gmail.com>
2014-11-24 12:31:30 -0500

    Add null checks for report fields

--- db/migrate/20141121152317_create_reports.rb
+++ db/migrate/20141121152317_create_reports.rb
@@ -1,9 +1,9 @@
 class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
-    	t.string :state
-    	t.string :person
-    	t.text :content
+    	t.string :state, null: false
+    	t.string :person, null: false
+    	t.text :content, null: false
     	t.belongs_to :user
     	
     	t.timestamps
