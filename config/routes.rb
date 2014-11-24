be4639459cf9cc0fe35387c93c7c695914cc1c21
Katherine McClintic <kdmcclin@gmail.com>
2014-11-24 14:37:09 -0500

    Remove done tasks from comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,8 @@ Rails.application.routes.draw do
 
   get '/logout' => 'sessions#destroy'
 
-  #TODO: add sessions controller
-
   # You can have the root of your site routed with "root"
-  root 'welcome#index' #TODO : add welcome controller
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
