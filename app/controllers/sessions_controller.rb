7090b369c9c73a9ef3bf0aa9536ca52afc350199
Katherine McClintic <kdmcclin@gmail.com>
2014-11-24 14:09:38 -0500

    Update session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,5 +5,25 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		@user = User.find_by(email: session_params[:email])
+		.try(:authenticate, session_params[:password])
+		if @user
+			session[:user_id] = user.id
+			redirect_to '/'
+		else
+			flash[:error] = "Login failed!"
+			redirect_to login_path
+		end
+	end
+
+	def destroy
+		session.clear
+		redirect_to '/'
+	end
+
+	private
+
+	def session_params
+		params.require(:session).permit(:email, :password)
 	end
 end
