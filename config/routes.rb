488973ab6f993c0ac6d01bd79a677dbf38f718bc
Katherine McClintic <kdmcclin@gmail.com>
2014-12-16 10:29:52 -0500

    Add links page route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get '/logout' => 'sessions#destroy'
 
+  get '/links' => 'welcome#links'
+
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
